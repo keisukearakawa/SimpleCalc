99c8b79d1fcc4f7ddc4f366c41d4451d249e1fd8
keisukearakawa <bcka1220031@gn.iwasaki.ac.jp>
2013-01-09 10:25:04 +0900

    応用プログラミングAの課題1
    C#で作った簡単計算プログラム

---
+++ SimpleCalc/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace SimpleCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Input1TextBox = new System.Windows.Forms.TextBox();
+            this.PlusLabel = new System.Windows.Forms.Label();
+            this.Input2TextBox = new System.Windows.Forms.TextBox();
+            this.EqualLabel = new System.Windows.Forms.Label();
+            this.AnswerTextBox = new System.Windows.Forms.TextBox();
+            this.CalcButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Input1TextBox
+            // 
+            this.Input1TextBox.Location = new System.Drawing.Point(13, 13);
+            this.Input1TextBox.Name = "Input1TextBox";
+            this.Input1TextBox.Size = new System.Drawing.Size(100, 19);
+            this.Input1TextBox.TabIndex = 0;
+            // 
+            // PlusLabel
+            // 
+            this.PlusLabel.AutoSize = true;
+            this.PlusLabel.Location = new System.Drawing.Point(120, 19);
+            this.PlusLabel.Name = "PlusLabel";
+            this.PlusLabel.Size = new System.Drawing.Size(17, 12);
+            this.PlusLabel.TabIndex = 1;
+            this.PlusLabel.Text = "＋";
+            // 
+            // Input2TextBox
+            // 
+            this.Input2TextBox.Location = new System.Drawing.Point(144, 12);
+            this.Input2TextBox.Name = "Input2TextBox";
+            this.Input2TextBox.Size = new System.Drawing.Size(100, 19);
+            this.Input2TextBox.TabIndex = 2;
+            // 
+            // EqualLabel
+            // 
+            this.EqualLabel.AutoSize = true;
+            this.EqualLabel.Location = new System.Drawing.Point(251, 19);
+            this.EqualLabel.Name = "EqualLabel";
+            this.EqualLabel.Size = new System.Drawing.Size(17, 12);
+            this.EqualLabel.TabIndex = 3;
+            this.EqualLabel.Text = "＝";
+            // 
+            // AnswerTextBox
+            // 
+            this.AnswerTextBox.Location = new System.Drawing.Point(275, 12);
+            this.AnswerTextBox.Name = "AnswerTextBox";
+            this.AnswerTextBox.Size = new System.Drawing.Size(100, 19);
+            this.AnswerTextBox.TabIndex = 4;
+            // 
+            // CalcButton
+            // 
+            this.CalcButton.Location = new System.Drawing.Point(12, 59);
+            this.CalcButton.Name = "CalcButton";
+            this.CalcButton.Size = new System.Drawing.Size(358, 25);
+            this.CalcButton.TabIndex = 5;
+            this.CalcButton.Text = "計算する";
+            this.CalcButton.UseVisualStyleBackColor = true;
+            this.CalcButton.Click += new System.EventHandler(this.CalcButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 96);
+            this.Controls.Add(this.CalcButton);
+            this.Controls.Add(this.AnswerTextBox);
+            this.Controls.Add(this.EqualLabel);
+            this.Controls.Add(this.Input2TextBox);
+            this.Controls.Add(this.PlusLabel);
+            this.Controls.Add(this.Input1TextBox);
+            this.Name = "Form1";
+            this.Text = "簡単計算プログラム";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Input1TextBox;
+        private System.Windows.Forms.Label PlusLabel;
+        private System.Windows.Forms.TextBox Input2TextBox;
+        private System.Windows.Forms.Label EqualLabel;
+        private System.Windows.Forms.TextBox AnswerTextBox;
+        private System.Windows.Forms.Button CalcButton;
+    }
+}
+
---
+++ SimpleCalc/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SimpleCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcButton_Click(object sender, EventArgs e)
+        {
+            //変数の宣言
+            int valueLeft;      //入力値１用の整数型変数
+            int valueRight;     //入力値２用の整数型変数
+            int valueAnswer;    //計算結果用整数型変数
+            //値の取り込み
+            valueLeft = InputCheck(Input1TextBox.Text,out valueLeft);
+                                //入力値１を整数型に変換後代入
+            valueRight = InputCheck(Input2TextBox.Text, out valueRight);
+                                //入力値２を整数型に変換後代入
+            //取り込んだ値の計算
+            valueAnswer = valueLeft + valueRight;
+            //計算結果を出力
+            AnswerTextBox.Text = valueAnswer.ToString();
+                                //文字列に変換後代入
+        }
+        private int InputCheck(string textValue, out int value)
+        {
+            if (int.TryParse(textValue, out value) == true)
+            {
+                value = int.Parse(textValue);
+            }
+            else
+            {
+                value = 0;
+            }
+
+            return value;
+        }
+    }
+}
