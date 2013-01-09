573784a7ec4b680c2674b4785d7211c4cfe47b10
keisukearakawa <bcka1220031@gn.iwasaki.ac.jp>
2013-01-09 10:46:39 +0900

    課題１
    掛け算の実�

--- SimpleCalc/Form1.cs
+++ SimpleCalc/Form1.cs
@@ -65,5 +65,23 @@ namespace SimpleCalc
             AnswerTextBox.Text = valueAnswer.ToString();
             //文字列に変換後代入
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //変数の宣言
+            int valueLeft;      //入力値１用の整数型変数
+            int valueRight;     //入力値２用の整数型変数
+            int valueAnswer;    //計算結果用整数型変数
+            //値の取り込み
+            valueLeft = InputCheck(Input1TextBox.Text, out valueLeft);
+            //入力値１を整数型に変換後代入
+            valueRight = InputCheck(Input2TextBox.Text, out valueRight);
+            //入力値２を整数型に変換後代入
+            //取り込んだ値の計算
+            valueAnswer = valueLeft * valueRight;
+            //計算結果を出力
+            AnswerTextBox.Text = valueAnswer.ToString();
+            //文字列に変換後代入
+        }
     }
 }
